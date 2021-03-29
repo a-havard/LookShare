9ce6068a160645b8d8ef1c5429aa7d9ea997e471
Nathan Wesley <nathanswesley@gmail.com>
2021-03-29 04:56:00 -0500

    take out comments

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,16 +2,15 @@ import React from 'react'
 import './App.css'
 import { BrowserRouter as Router, Route} from 'react-router-dom'
 
+import SignIn from './components/SignIn'
 import SignUp from './components/SignUp'
 
 const App = props => {
   return(
     <div className="App">
       <Router>
+        <Route exact path="/" component ={SignIn} />
         <Route exact path="/signup" component={SignUp} />
-        {/* <Route exact path="/" component={Content} /> */}
-        {/* <Route exact path="/poll/response/:id" component={PollResponseForm} /> */}
-        {/* <Route path="/poll/results/:id" component={PollResults} /> */}
       </Router>
     </div>
   )
