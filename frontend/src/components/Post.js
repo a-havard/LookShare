f168befd6a2f2fef079403ba75846c61fb4c3a83
will-dunn <wjdunn@smu.edu>
2021-04-21 05:32:28 -0500

    started comment implementation

---
+++ frontend/src/components/Post.js
@@ -0,0 +1,242 @@
+import React, {useEffect, useState } from 'react';
+import Button from '@material-ui/core/Button'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import TextField from '@material-ui/core/TextField'
+import Link from '@material-ui/core/Link'
+import Grid from '@material-ui/core/Grid'
+import Typography from '@material-ui/core/Typography'
+import { makeStyles } from '@material-ui/core/styles'
+import Container from '@material-ui/core/Container'
+import { green } from '@material-ui/core/colors';
+import Paper from '@material-ui/core/Paper';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import { Popover } from '@material-ui/core'
+import PostAPIs from '../routes/postAPIs';
+import Profile from '../routes/profile';
+import { matchPath, useParams } from 'react-router';
+import Axios from 'axios';
+import {Rating} from '../Rating';
+import CommentForm from './CommentForm'
+import { conn } from '../routes/config';
+const useStyles = makeStyles((theme) => ({
+    profilepicgrid:{
+      marginLeft: theme.spacing(4),
+    },
+    editButton:{
+      backgroundColor:"brown",
+      padddingTop: theme.spacing(1),
+    },
+    grid:{
+      marginTop: theme.spacing(0),
+      marginLeft: theme.spacing(1)
+      
+    },
+    logo:{
+      float: "center",
+      marginLeft: theme.spacing(0),
+    },
+    picture:{
+      width: "90%",
+      paddingBottom: theme.spacing(2),
+    },
+    paper: {
+      marginTop: theme.spacing(0),
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
+    avatar: {
+      margin: theme.spacing(1),
+      backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+      width: '100%',
+      marginTop: theme.spacing(3),
+    },
+    submit: {
+      margin: theme.spacing(3, 0, 2),
+    },
+    postPicture:{
+      height:'25vh'
+    },
+    profilePic:{
+      borderRadius: '50%',
+    },
+    forms:{
+      width:'100%',
+      height:'100%',
+      border: 10
+      
+    },
+    formControl:{
+     
+      width:'85%'
+    },
+    formButton:{
+     
+      width:'85%'
+    },
+    like:{
+        height: '10vh'
+    }
+
+    
+  }))
+const Post=props=>{
+    let ratings=[1,2,3,4,5,6,7,8,9,10];
+    let [formData, setFormData]=useState({
+        name: '',
+        rating: '',
+        comment:''
+    });
+
+    console.log(props);
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const classes = useStyles();
+    let [openPost,setOpen]=useState();
+    let instructions=props.post.instructions.split(',');
+    let products=props.post.products.split(',');
+      const handleClose = () => {
+        setAnchorEl(null);
+        setOpen(false);
+      };
+     
+      return (<Card>
+        <CardActionArea onClick={()=>{setOpen(true);}}>
+       
+        <CardContent>
+          <ShowImg val={props.post.photo}/>
+          </CardContent>
+      </CardActionArea>
+     
+<Popover 
+        className={classes.overlay}
+        
+        open={openPost}
+        anchorPosition={{left: '0vw',top: '0vh'}
+        }
+    
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'center'
+        }}
+        transformOrigin={{
+          horizontal: 'center'
+        }}
+        
+        PaperProps={{
+        style: { width: '70%',
+        height:'70vh'},
+        }}
+      >
+          <Grid container  maxWidth="80vw" className={classes.grid} spacing={2}>
+              <Grid item xs={12}>
+                <ShowImg val={props.post.photo}/>
+              </Grid>
+              <Grid item xs={12}>
+                  <p>instructions</p>
+                <ol>
+                {
+                    instructions.map((x, i) => <li className="list-group-item" key={ i }>
+                        {x}
+                    </li>)
+                }
+                </ol>
+              </Grid>
+              <Grid item xs={4}>
+                <p>Type of look:{props.post.lookKind}</p>
+              </Grid>
+              <Grid item xs={4}>
+                <p>Time for this look: {props.post.lookTime} minutes</p>
+              </Grid>
+              <Grid item xs={4}>
+                <p>Difficulty: {props.post.lookDifficulty}/10</p>
+              </Grid>
+              <Grid item xs={12}>
+              <p>products</p>
+                <ul>
+                {
+                    products.map((x, i) => <li className="list-group-item" key={ i }>
+                        {x}
+                    </li>)
+                }
+                </ul>
+              </Grid>
+              <Grid item xs={12}>
+                  <form>
+                  <label htmlFor="comment">Comment</label>
+                    <textarea
+                        id="comment"
+                       name="comment"
+                       value={formData.comment}
+                       onChange={event => setFormData({comment: event.target.value})}
+                       className="form-control"/>
+                       <button
+                    type="button"
+                    className="btn btn-primary btn-block"
+                    onClick={ () => onAddClick() }>CRASHES BACKEND! LOCALLY?</button>
+                  </form>
+                  </Grid>
+          </Grid>
+         
+        
+      </Popover>
+      </Card>)
+      
+    function onAddClick(){
+        let rating={
+            authorId: props.post.authorId,
+            parentPostId:props.post.parentPostId,
+            parentCommentId:null,
+            isRepost:false,
+            restricted:false
+        }
+        conn.post('/comments/comment',{
+            authorId: props.post.authorId,
+            parentPostId:null,
+            parentCommentId:null,
+            comment: formData.comment,
+            isRepost:false,
+            restricted:false
+        }).then((res)=>console.log(res));
+    }  
+
+
+    function ShowImg(val){
+        //console.log(val);
+        const [pic, setPic] = useState('');
+       
+          useEffect(()=>{
+            if(!pic){
+              bufferToImage();
+            }
+          },[]);
+          const bufferToImage= async ()=>{
+            var arrayBufferView = new Uint8Array( val.val.data );
+            var blob = new Blob( [arrayBufferView]);
+          
+           var imageUrl = URL.createObjectURL( blob );
+           var reader = new FileReader();
+           let y;
+           reader.onload = function() {
+              // alert(reader.result);
+              //console.log(reader.result);
+               setPic(reader.result);
+               
+           }
+           reader.readAsText(blob);
+          }
+       return <img src={pic} className={classes.postPicture}/>;
+        
+      }
+    return <p>filler</p>;
+   
+}
+export default Post;
---
+++ frontend/src/components/ProfilePage.js
@@ -0,0 +1,669 @@
+import React, {useEffect, useState } from 'react';
+import Button from '@material-ui/core/Button'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import TextField from '@material-ui/core/TextField'
+import Link from '@material-ui/core/Link'
+import Grid from '@material-ui/core/Grid'
+import Typography from '@material-ui/core/Typography'
+import { makeStyles } from '@material-ui/core/styles'
+import Container from '@material-ui/core/Container'
+import { green } from '@material-ui/core/colors';
+import Paper from '@material-ui/core/Paper';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import { Popover } from '@material-ui/core'
+import PostAPIs from '../routes/postAPIs';
+import Profile from '../routes/profile';
+import { matchPath, useParams } from 'react-router';
+import Axios from 'axios';
+import Post from './Post';
+
+import {conn} from '../routes/config'
+
+const useStyles = makeStyles((theme) => ({
+    profilepicgrid:{
+      marginLeft: theme.spacing(4),
+    },
+    editButton:{
+      backgroundColor:"brown",
+      padddingTop: theme.spacing(1),
+    },
+    grid:{
+      marginTop: theme.spacing(0),
+      marginLeft: theme.spacing(1)
+      
+    },
+    logo:{
+      float: "center",
+      marginLeft: theme.spacing(0),
+    },
+    picture:{
+      width: "90%",
+      paddingBottom: theme.spacing(2),
+    },
+    paper: {
+      marginTop: theme.spacing(0),
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
+    avatar: {
+      margin: theme.spacing(1),
+      backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+      width: '100%',
+      marginTop: theme.spacing(3),
+    },
+    submit: {
+      margin: theme.spacing(3, 0, 2),
+    },
+    postPicture:{
+      width: '100%',
+    },
+    profilePic:{
+      borderRadius: '50%',
+    },
+    forms:{
+      width:'100%',
+      height:'100%',
+      border: 10
+      
+    },
+    formControl:{
+     
+      width:'85%'
+    },
+    formButton:{
+     
+      width:'85%'
+    },
+
+    
+  }))
+
+ 
+ 
+var loaded=false;
+const ProfilePage =()=>{
+  console.log("test");
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  //const [params,setParams]=React.useState(null);
+var par=useParams();
+  let fileInput = React.createRef(); 
+  let temp;
+  let [username,setUsername]=useState(null);
+  let [bio,setBio]=useState('');
+  let [bioLink,setBioLink]=useState('');
+  let [followers,setFollowers]=useState([]);
+  let [following,setFollowing]=useState([]);
+  let [posts,setPosts]=useState([]);
+  let [popOpen,setPopOpen]=useState();
+  let [addingPost,setPosting]=useState();
+  let [selectedPost,setSelectedPost]=useState();
+  const [showFollowerList,setSFL]=useState();
+  const [showFollowingList,setSFLL]=useState();
+  const [dataUri, setDataUri] = useState('');
+  const [values, setValues] = useState([]);
+
+ 
+useEffect(() => {
+  console.log(par);
+  var id=parseInt(par.id);
+  var logged=''+localStorage.getItem('loggedInId');
+  console.log(logged);
+  if (!username) {
+   
+    conn.get("/accounts/"+par.id,{params:{loggedInId : logged}})
+    .then((res) => {
+        //console.log(res.data);
+
+   
+        setUsername(''+res.data.data.username);
+        setBio(''+res.data.data.bio);
+        setBioLink(''+res.data.data.bioLink);
+        if(res.data.data.followers)
+        setFollowers(res.data.data.followers);
+        if(res.data.data.following)
+        setFollowing(res.data.data.following);
+       
+        
+        loaded=true;
+      })
+      .catch((res) => {
+      
+      })
+  conn.get("/posts/authors/"+par.id,{params:{loggedInId : localStorage.getItem('loggedInId')}})
+    .then((res) => {
+      //console.log(res.data.data);
+      setPosts(res.data.data);
+      
+   
+      //console.log(posts);
+      var pics=[];
+      let i=0;
+    
+
+    
+    });
+    
+   
+   
+  }
+});
+ function openFollowers(){
+   setSFL(true);
+ }
+ function openFollowing(){
+  setSFLL(true);
+}
+  function getPictures(){
+    console.log(posts);
+    let pics=[];
+    let i=0;
+  for( i=0;i<posts.length;i++){
+    console.log(posts[i].photo);
+    var arrayBufferView = new Uint8Array( posts[i].photo.data );
+      var blob = new Blob( [arrayBufferView]);
+  
+      var imageUrl = URL.createObjectURL( blob );
+      var reader = new FileReader();
+      let y;
+      reader.onload = function() {
+         // alert(reader.result);
+          y=reader.result;
+          pics.push(y);
+      }
+      reader.readAsText(blob);
+  }
+  setValues(pics);
+  //console.log(pics);
+  }
+  /*
+  */
+  const classes = useStyles();
+    //let numPictures
+  function FollowerList(){
+    const handleClick = (event) => {
+      setAnchorEl(event.currentTarget);
+      console.log("click");
+      if(!showFollowerList)
+        setSFL(true);
+      
+    };
+  
+    const handleClose = () => {
+      setAnchorEl(null);
+      setSFL(false);
+    };
+  
+    const open = showFollowerList;
+    const id = open ? 'simple-popover' : undefined;
+    let pop=<div><Button aria-describedby={id} variant="contained" color="primary" onClick={handleClick}>
+    Followers: {followers.length}
+  </Button>
+    <Popover
+      id={id}
+      open={showFollowerList}
+      anchorEl={anchorEl}
+      onClose={handleClose}
+      anchorOrigin={{
+        vertical: 'top',
+        horizontal: 'left',
+      }}
+      transformOrigin={{
+        vertical: 'top',
+        horizontal: 'center',
+      }}
+    >
+        <ul className="list-group">
+        {
+            followers.map((x, i) => <div><a href={'/profile/'+x.userId}><button onClick={()=>{console.log(x)}}>{x.userId }</button></a><br/></div>
+         )
+        }
+     </ul>
+    </Popover></div>;
+    return (
+      pop
+    ); 
+    
+  }
+  function FollowingList(){
+    const handleClick = (event) => {
+      setAnchorEl(event.currentTarget);
+      console.log("click");
+      if(!showFollowingList)
+        setSFLL(true);
+      
+    };
+  
+    const handleClose = () => {
+      setAnchorEl(null);
+      setSFLL(false);
+    };
+  
+    const open = showFollowingList;
+    const id = open ? 'simple-popover' : undefined;
+    let pop=<div><Button aria-describedby={id} variant="contained" color="primary" onClick={handleClick}>
+    Following: {following.length}
+  </Button>
+    <Popover
+      id={id}
+      open={showFollowingList}
+      anchorEl={anchorEl}
+      onClose={handleClose}
+      anchorOrigin={{
+        vertical: 'top',
+        horizontal: 'left',
+      }}
+      transformOrigin={{
+        vertical: 'top',
+        horizontal: 'center',
+      }}
+    >
+        <ul className="list-group">
+        {
+            following.map((x, i) => <div><a href={'/profile/'+x.userId}><button onClick={()=>{console.log(x)}}>{x.userId }</button></a><br/></div>
+         )
+        }
+     </ul>
+    </Popover></div>;
+    return (
+      pop
+    ); 
+    
+  }
+  function updateBio (newBio,newLink){
+    setBio(newBio);
+    setBioLink(newLink);
+    conn.put("/accounts/"+par.id+"/bio",{bio: newBio, bioLink:newLink})
+    .then((res)=>{console.log(res)})
+    .then(setPopOpen(false));
+  }
+  function BioForm(){
+    
+     let temp='';
+     let temp2='';
+    const [anchorEl, setAnchorEl] = React.useState(null);
+
+    const handleClick = (event) => {
+      setAnchorEl(event.currentTarget);
+      if(!popOpen)
+        setPopOpen(true);
+      
+    };
+  
+    const handleClose = () => {
+      setAnchorEl(null);
+      setPopOpen(false);
+    };
+  
+    const open = Boolean(anchorEl);
+    const id = open ? 'simple-popover' : undefined;
+    let pop=<div><Button aria-describedby={id} variant="contained" color="primary" onClick={handleClick}>
+    Edit Bio
+  </Button>
+    <Popover
+      id={id}
+      open={popOpen}
+      anchorEl={anchorEl}
+      onClose={handleClose}
+      anchorOrigin={{
+        vertical: 'top',
+        horizontal: 'center',
+      }}
+      transformOrigin={{
+        vertical: 'top',
+        horizontal: 'center',
+      }}
+      PaperProps={{
+        style: { width: '70%',
+        height:'70vh'},
+        }}
+    >
+      <form>
+      <label for="link">Link</label>
+      <input
+                        type="text"
+                        name="link"
+                        id="link"
+                        className="form-control"
+                        
+                        onChange={ event => {temp2=( event.target.value )} } 
+                        />
+    <br/>
+    <label for="Bio">Bio</label>
+     <input
+                        type="text"
+                        name="Bio"
+                        id="Bio"
+                        className="form-control"
+                        
+                        onChange={ event => {temp=( event.target.value )} } 
+                        />
+                        <br/>
+      <button
+                            type="button"
+                            className="btn btn-success btn-block"
+                            onClick={ () => updateBio(temp,temp2) }>
+                            Add
+                        </button>            
+     </form>
+    </Popover></div>;
+    if(par.id!=localStorage.getItem('loggedInId'))
+      return <></>;
+    return (
+      pop
+    );
+  }
+
+  function addPost (formData){
+    //console.log(file);
+    let reader = new FileReader();
+    let inFile = formData.file;
+    if(!inFile){
+      alert("No Photo");
+      return;
+    }
+    //console.log(inFile);
+    //console.log("add Post");
+    reader.onloadend = () => {
+      console.log(reader.result);
+      let t= {authorId: par.id,
+        instructions: formData.instructions,
+        lookDifficulty: formData.lookDifficulty,
+        lookKind: formData.lookKind,
+        lookTime: formData.lookTime,
+        photo: reader.result,
+        products: formData.products,
+       }
+        
+      
+        conn.post('/posts/post',t)
+          .then((res)=>{console.log(res)
+            conn.get("/posts/authors/"+par.id,{params:{loggedInId : localStorage.getItem('loggedInId')}})
+          .then((res) => {
+            //console.log(res.data.data);
+             t=res.data.data;
+            
+            setPosts(t);
+          });});
+        
+        
+   };
+  reader.readAsDataURL(inFile);
+
+    
+  }
+  function PostInformationForm(){
+    
+    
+    
+    const handleClick = (event) => {
+      setAnchorEl(event.currentTarget);
+      if(!popOpen)
+      setPosting(true);
+      
+    };
+  
+   
+  
+    const open = Boolean(anchorEl);
+    const id = open ? 'simple-popover' : undefined;
+    let pop=<div><Button aria-describedby={id} variant="contained" color="primary" onClick={handleClick}>
+    Add New Post
+  </Button>
+    </div>;
+     if(par.id!=localStorage.getItem('loggedInId'))
+     return <></>;
+    return (
+      pop
+    );
+  }
+  const handleClick = (event) => {
+    //setAnchorEl(event.currentTarget);
+    if(!popOpen)
+    setPosting(true);
+    
+  };
+    function PostingPopover(){
+      /*let file='';
+      let instructionlist='';
+      let lookkind='';
+      let difficulty=0;*/
+      let formData= {
+        instructions: "",
+        lookDifficulty: 1,
+        lookKind: "",
+        lookTime: 0,
+        file: '',
+        products: "",
+       };
+    
+      let ratings=[1,2,3,4,5,6,7,8,9,10];
+    const handleClose = () => {
+      setAnchorEl(null);
+      setPosting(false);
+    };
+    if(par.id!=localStorage.getItem('loggedInId'))
+    return <></>;
+      return <Popover 
+      className={classes.overlay}
+      
+      open={addingPost}
+      anchorPosition={{left: '0vw',top: '0vh'}
+      }
+  
+      onClose={handleClose}
+      anchorOrigin={{
+        vertical: 'top',
+        horizontal: 'center'
+      }}
+      transformOrigin={{
+        horizontal: 'center'
+      }}
+      
+      PaperProps={{
+      style: { width: '70%',
+      height:'70vh'},
+      }}
+    >
+     <h1>Please Enter the information for your new post</h1>
+     <form className={classes.forms}>
+     <Grid container  maxWidth="80vw" className={classes.grid} spacing={2}>
+      <Grid item xs={12} >
+        <label for='photo'>Select Photo:   </label><br/>
+      <input
+                        type="file"
+                        name="photo"
+                        id="photo"
+                        className={classes.formControl}
+                        //onChange={handleImageChange}
+                        ref={fileInput}
+                        onChange={ event => {formData.file=( event.target.files[0])} } 
+                    
+                        />
+                        </Grid>
+      <Grid item xs={12} >
+        <label for='instructions'>Please List Your Instructions:  </label><br/>
+      <input
+                        type="text"
+                        name="Instructions"
+                        id="instructions"
+                        className={classes.formControl}
+                        ref={fileInput}
+                        onChange={ event => {formData.instructions=(event.target.value)} } 
+                    
+                        />
+                        </Grid>
+          <Grid item xs={5} >
+        <label for='kind'>What type of look:  </label><br/>
+      <input
+                        type="text"
+                        name="kind"
+                        id="kind"
+                        className={classes.formControl}
+                        
+                        ref={fileInput}
+                        onChange={event => {formData.lookKind=( event.target.value)}} 
+                    
+                        />
+                        </Grid>
+          <Grid item xs={5} >
+            <label for='products'>Time to do in minutes:  </label><br/>
+              <input
+                        type="number"
+                        min={0}
+                        name="products"
+                        id="products"
+                        className={classes.formControl}
+                        //onChange={handleImageChange}
+                        ref={fileInput}
+                        onChange={event => {formData.products=( event.target.value)}} 
+                    
+                        />
+                        </Grid>                        
+        <Grid item xs={1} >
+        <label for='difficulty'>Look Difficulty:  </label><br/>
+            <select
+                        type="select"
+                        name="difficulty"
+                        id="difficulty"
+                        className={classes.formControl}
+                    
+                        ref={fileInput}
+                        onChange={ event => {formData.lookDifficulty=(event.target.value )} } 
+                    
+                        >
+                         {
+                                    ratings.map((x, i) => <option key={ i }>{ x }</option>)
+                                }</select>
+                        </Grid>
+          
+                        
+      <Grid item xs={12} >
+        <label for='products'>Products:  </label><br/>
+        <input
+                        type="text"
+                        name="products"
+                        id="products"
+                        className={classes.formControl}
+                        //onChange={handleImageChange}
+                        ref={fileInput}
+                        onChange={event => {formData.products=( event.target.value)}} 
+                    
+                        />
+                        </Grid>
+                        
+     
+                        <Grid item xs={12} >
+                        <button
+                            type="button"
+                            className={classes.formButton}
+                            onClick={ () => addPost(formData) }>
+                            Post it
+                        </button> 
+                          </Grid>                 
+      </Grid>
+     
+   
+                
+     </form>
+    </Popover>
+    }
+    
+    function ShowImg(val){
+      //console.log(val);
+      const [pic, setPic] = useState('');
+     
+        useEffect(()=>{
+          if(!pic){
+            bufferToImage();
+          }
+        },[]);
+        const bufferToImage= async ()=>{
+          var arrayBufferView = new Uint8Array( val.val.data );
+          var blob = new Blob( [arrayBufferView]);
+        
+         var imageUrl = URL.createObjectURL( blob );
+         var reader = new FileReader();
+         let y;
+         reader.onload = function() {
+            // alert(reader.result);
+            //console.log(reader.result);
+             setPic(reader.result);
+             
+         }
+         reader.readAsText(blob);
+        }
+     return <img src={pic} className={classes.postPicture}/>;
+      
+    }
+    function FormRow() {
+      let items=[];
+      
+      
+      return <Grid  item container xs={100} spacing={0} >{
+        posts.map((x,i)=> 
+        <Grid item xs={4} key={ i }>
+          <Post post={x}/>
+          </Grid>)
+         
+    }</Grid>
+  
+     
+     
+    }
+   
+    return(
+      <>
+    
+      <PostingPopover/>
+      <Grid container component="main" maxWidth="80vw" className={classes.grid} spacing={2}> 
+       
+      <Grid className={classes.logo} item xs={12}>
+      
+      
+      </Grid>
+     
+      <Grid className={classes.profilepicgrid } item xs={2} rs={3} spacing={30}>
+        <img src="https://via.placeholder.com/150" className={classes.profilePic}></img>
+    
+      </Grid>
+      <Grid id="info" item container xs={4}  spacing={1}>
+        <Grid item xs={5} >
+          <Paper className={classes.paper}>{username}</Paper>
+        </Grid>
+        <Grid item xs={5} >
+          <BioForm/>
+        </Grid>
+        <Grid item xs={5} rs={1}>
+          <Paper className={classes.paper}><FollowerList/></Paper>
+        </Grid>
+        <Grid item xs={5} rs={1}>
+          <Paper className={classes.paper}><FollowingList/></Paper>
+        </Grid>
+   
+    </Grid>
+    
+   
+   
+    <Grid item xs={5} rs={3}>
+      
+      <Paper className={classes.paper}><a href={bioLink}>{bioLink}</a><p>{bio}</p></Paper>
+    </Grid>
+    <Grid item xs={12} rs={2}></Grid>
+    <FormRow/>
+    <Grid item xs={12} rs={2}><PostInformationForm/></Grid>
+    </Grid>
+    
+</>
+    );
+}
+
+export default ProfilePage
