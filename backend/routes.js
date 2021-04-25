243bb168f2b7b2c7d1b2291526dcbe5dc4502e0f
Andrew Havard <rocketboy@thehavards.net>
2021-04-25 14:32:06 -0500

    Added user search route

--- backend/routes.js
+++ backend/routes.js
@@ -98,6 +98,54 @@ module.exports = function routes(app, logger) {
     });
   });
 
+  app.get('/accountId', (req, res) => {
+    pool.getConnection((err, connection) => {
+      if (err) {
+        logger.error("Could not connecto to the database!", err);
+        return res.status(400).json({ 
+          "data": -1,
+          "message": "Could not connect to the database!"
+        });
+      }
+
+      let validInformation = requireQueryParams(req, ["username"]);
+      if (!validInformation) {
+        connection.release();
+        logger.error("Could not connect to the database!", err);
+        return res.status(200).json({
+          "data": -1,
+          "message": "Need to pass username to API through request body!"
+        });
+      }
+
+      let username = req.query.username;
+      let sql = `SELECT userId FROM Accounts WHERE username = "${username}"`;
+      connection.query(sql, (err, rows, fields) => {
+        if (err) {
+          connection.release();
+          logger.error("Could not connect to the database!", err);
+          return res.status(200).json({
+            "data": -1,
+            "message": "Need to pass username to API through request body!"
+          });
+        }
+
+        connection.release();
+        if (rows.length === 0) {
+          return res.status(200).json({
+            "data": -1,
+            "message": "No user exists with that username!"
+          });
+        }
+
+        return res.status(200).json({
+          "data": rows[0].userId,
+          "message": "Found a userId for that username!"
+        })
+      })
+    });
+  });
+
   app.post('/accounts', async (req, res) => {
     pool.getConnection((err, connection) => {
       // Try to connect to database, return an error if cannot
@@ -409,7 +457,7 @@ module.exports = function routes(app, logger) {
       //comment posted successfully
       
     });
-  }); 
+  });
 
   //Get Comments from a post
   app.get('/comments/posts/:postId', async(req, res) => {
@@ -422,21 +470,17 @@ module.exports = function routes(app, logger) {
         });
       } else {
         let postId = typeof req.params.postId === "string" ? JSON.parse(req.params.postId) : req.params.postId;
-        console.log(postId);
-        let sql = `SELECT * FROM Comments WHERE parentPostId = ${postId}`;
+        let sql = `SELECT * FROM Comments WHERE postId = ${postId}`;
 
         connection.query(sql, function (err, rows, fields) {
-          
+          connection.release();
           if (err) {
-            connection.release();
             logger.error("Error while fetching values: \n", err);
             res.status(400).json({
               "data": -1,
               "error": "Error obtaining values"
             })
           } else {
-            connection.release();
-            console.log(rows);
             res.status(200).json({
               "data": rows,
             });
